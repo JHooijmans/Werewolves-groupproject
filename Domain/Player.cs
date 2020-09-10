e0ffadccb52119ce637fa8b140caef1168ba0cc7
Olmo <okortenbosch@sogyo.nl>
2020-09-10 20:35:44 +0200

    "Added basic properties to the player class."

--- Domain/Player.cs
+++ Domain/Player.cs
@@ -4,6 +4,20 @@ namespace Domain
 {
     public class Player
     {
-        
+        string name;
+        bool alive = true;
+        int votes = 0;
+
+        public void vote() {
+            this.votes += 1;
+        }
+
+        public void resetVotes() {
+            this.votes = 0;
+        }
+
+        public string getName() {
+            return this.name;
+        }
     }
 }
