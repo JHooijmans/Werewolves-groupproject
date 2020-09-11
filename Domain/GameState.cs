db9a09325af468ed330b6875983439b37ffa1fd9
Olmo <okortenbosch@sogyo.nl>
2020-09-11 21:27:18 +0200

    "Added a Shuffle function to randomise roles"

--- Domain/GameState.cs
+++ Domain/GameState.cs
@@ -15,14 +15,46 @@ namespace Domain
             this.day = !this.day;
         }
         
-        ArrayList players = new ArrayList();
+        List<string> playerNames = new List<string>();
 
-        public ArrayList getPlayers(){
+        Player[] players;
+
+        public Player[] getPlayers(){
             return this.players;
         }
 
-        public void addPlayer(Player player) {
-            this.players.Add(player);
+        public List<string> getPlayerNames() {
+            return this.playerNames;
+        }
+
+        public void addPlayerName(string playerName) {
+            this.playerNames.Add(playerName);
+        }
+
+        string[] Shuffle(List<string> namesList) {
+            int counter = 0;
+            string[] namesArray = new string[namesList.Count];
+            foreach (string name in namesList)
+            {
+                namesArray[counter] = name;
+                counter++;
+            }
+            string tempString = "";
+            for (i = namesList.Count -1; i > 0; i--)
+            {
+                int j = Random.Next(i + 1);
+                tempString = namesArray[i];
+                namesArray[i] = namesArray[j];
+                namesArray[j] = temp;
+            }
+            return namesArray;
+        }
+
+        public void GameStart() {
+            int length = this.playerNames.Count;
+            this.players = new Player[length];
+            string[] names = Shuffle(this.playerNames);
+            
         }
     }
 }
