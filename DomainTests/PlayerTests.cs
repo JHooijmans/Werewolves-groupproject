5bef9db198c3558e5ab8f0a15d0eaef0d4aeb32c
Bram Smit <bsmit@sogyo.nl>
2020-09-22 15:16:49 +0200

    Adds Player init test.

--- DomainTests/PlayerTests.cs
+++ DomainTests/PlayerTests.cs
@@ -7,5 +7,27 @@ namespace DomainTests
     [TestFixture]
     class PlayerTest 
     {
+
+        [TestCase("Werewolf")]
+        [TestCase("Villager")]
+        public void TestPlayerInitialization(string roleName)
+        {
+            Role role;
+            if (roleName == "Werewolf")
+            {
+                role = new Werewolf();
+            }
+            else
+            {
+                role = new Villager();
+            }
+
+            Player player = new Player("test", role);
+            Assert.AreEqual("test", player.getName());
+            Assert.AreEqual(role.GetType().Name, player.getRole().GetType().Name);
+            Assert.IsTrue(player.getVotes() == 0);
+            Assert.IsFalse(player.hasVoted());
+            Assert.IsTrue(player.checkPulse());
+        }
     }
 }
