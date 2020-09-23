95c4a34928b8ed0ab79dd9b7d002d5bf23792935
Bram Smit <bsmit@sogyo.nl>
2020-09-23 22:54:16 +0200

    Adds unit tests for player class.

--- DomainTests/PlayerTests.cs
+++ DomainTests/PlayerTests.cs
@@ -10,7 +10,7 @@ namespace DomainTests
 
         [TestCase("Werewolf")]
         [TestCase("Villager")]
-        public void TestPlayerInitialization(string roleName)
+        public void testPlayerInitialization(string roleName)
         {
             Role role;
             if (roleName == "Werewolf")
@@ -21,13 +21,52 @@ namespace DomainTests
             {
                 role = new Villager();
             }
-
             Player player = new Player("test", role);
             Assert.AreEqual("test", player.getName());
             Assert.AreEqual(role.GetType().Name, player.getRole().GetType().Name);
             Assert.IsTrue(player.getVotes() == 0);
             Assert.IsFalse(player.hasVoted());
             Assert.IsTrue(player.checkPulse());
+        }
+
+
+        [Test]
+        public void testSingleVillagerVote()
+        {
+            // Arrange
+            Player p1 = new Player("test1", new Villager());
+            Player p2 = new Player("test2", new Villager());
+            // Act
+
+            p1.vote(p2);
+
+            // Assert
+            Assert.IsTrue(p1.hasVoted());
+            Assert.IsFalse(p2.hasVoted());
+            Assert.AreEqual(p2.getVotes(), 1);
+            Assert.AreEqual(p1.getVotes(), 0);
+            Assert.AreSame(p2, p1.getVoteTarget());
+        }
+
+        [Test]
+        public void testResetVotes()
+        {
+            // Arrange
+            Player p1 = new Player("test1", new Villager());
+            Player p2 = new Player("test2", new Villager());
+            // Act
+
+            p1.vote(p2);
+            p1.resetVotes();
+            p2.resetVotes();
+
+            // Assert
+            Assert.IsTrue(p1.getVotes() == 0);
+            Assert.IsTrue(p2.getVotes() == 0);
+            Assert.IsFalse(p1.hasVoted());
+            Assert.IsFalse(p2.hasVoted());
+            Assert.IsTrue(p1.getVoteTarget() == null);
+            Assert.IsTrue(p2.getVoteTarget() == null);
         }
     }
 }
