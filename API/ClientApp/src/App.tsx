f74a33a56c71db1c1f26c58b0c4d4e8161a02cd0
jhooijmans <jhooijmans@sogyo.nl>
2020-09-28 14:02:26 +0200

    Refactoring changes to move relevant components from App to Game and made nickname field dissapear upon joining work

--- API/ClientApp/src/App.tsx
+++ API/ClientApp/src/App.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import "./App.css";
-import {Chat} from './components/chat/Chat';
 import {NickNameForm} from './components/NickNameForm';
 import {SignalRTime, SignalRClient} from './ConnectionService';
 import {Game} from './components/game/Game';
-import {StartGameButton} from './components/game/StartGameButton';
  
 
 const App: React.FC = () => {
   
  
     return (
-    <><SignalRTime /><SignalRClient /><NickNameForm /><Chat /> <Game /><StartGameButton /></>
+    <><SignalRTime /><SignalRClient /><NickNameForm /> <Game /></>
     
     );
 };
--- API/ClientApp/src/components/NickNameForm.tsx
+++ API/ClientApp/src/components/NickNameForm.tsx
@@ -6,8 +6,7 @@ export function NickNameForm () {
 
     const [nickName, setNickName] = useState<string>('');
     const [retryNick, setRetryNick] = useState<string>('');
-
-    let nameIsSet:boolean = false;
+    const [nameIsSet, toggleNameIsSet] = useState<boolean>(false);
 
     const handleNickName = async () => {
         if (nickName != '') {
@@ -16,25 +15,23 @@ export function NickNameForm () {
             if (nickTaken) {
                 setNickName('');
                 setRetryNick('The name you entered was already taken; please enter a new name.');
-            } else nameIsSet = true; 
+            } else toggleNameIsSet(true); 
         };
     };
 
-    // useEffect(() => {
-        
-    // });
 
 
-    if (nameIsSet === false) {
+    if (!nameIsSet) {
         return (
 
             <><input 
             type="text"
             value={nickName}
+            placeholder="Please enter your name:"
             onChange={e => setNickName(e.target.value)}
             maxLength={255}
             />
-            <button onClick={handleNickName}>Please enter your name:</button>
+            <button onClick={handleNickName}>Join Game</button>
             <p>{retryNick}</p>
             </>
         );
